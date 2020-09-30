22a1734c46be5a8ff0b9b13f82580563172fa4b7
Mehran Kazeminia <36100369+MehranKazeminia@users.noreply.github.com>
2020-09-30 20:34:15 +0330

    Create SmartCreator103.sol

---
+++ SmartCreator103.sol
@@ -0,0 +1,269 @@
+pragma solidity ^0.5.1;
+// Token creator contract using ERC20
+// & Block Producer Smart Contract (BPSC)
+// By: Somayyeh Gholami & Mehran Kazeminia
+
+contract SmartCreator103 {
+    string name;
+    string symbol;
+    uint256 totalValue;
+    uint256 priceOfCoin;    
+    uint8 decimals;
+    address owner;  
+    
+//  For example:
+//  name = "aka coin" ==> The name of the coins.
+//  symbol = "AKA" ==> Symbol of the coins.
+//  totalValue = 1000000 ==> Total number of coins.
+//  priceOfToken ==> The price of each Coin is based on Wei(Ether).
+//  decimals ==> Solidity supports integers but no decimals, so were coded a fixed point arithmetic contract.
+//  owner ==> The address of the account that first deploy this contract.
+
+    uint128 accountCount = 0; // To count the number of accounts.     
+    uint128 trxCount = 0; // To count the number of blocks.
+    bool lockTest = false; // For function locking.
+    
+    struct Account {
+        bool account;
+        uint256 accountBalance;
+        uint256 accountOpeningTime;        
+        uint256 lastTimeAsSender;
+    }
+    mapping(address=>Account) accounts;
+    mapping (address => mapping (address => uint256)) allowances;    
+    address[] accountsAddresses; //For storing all addresses.
+
+    struct Trx {
+        uint256 trxTime;        
+        uint256 trxValue;
+        address senderAddress;
+        address recipientAddress;
+    }  
+    Trx[] trxes;
+
+    modifier isMember() {
+        require(accounts[msg.sender].account, "This account has not been opened yet.");    
+        _;
+    }
+    modifier isLocked() {
+        require (! lockTest);
+        lockTest = true;
+        _;
+        lockTest = false;
+    }
+    
+    event NewAccountWasOpened();
+    event Transfer(address indexed, address indexed, uint256);
+    event Approval(address indexed, address indexed, uint256);
+    
+// __________________________________________________________________________________________
+//  The Owner that first deploys this contract.
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalValue) public {
+        require((msg.sender != address(0)) && (_totalValue > 0));        
+        
+        name = _name;
+        symbol = _symbol;
+        totalValue = _totalValue;
+        priceOfCoin = 1e12;
+        decimals = 0;
+        owner = msg.sender;
+
+        accounts[owner].account = true; // The first address became a member.
+        accounts[owner].accountBalance = totalValue;
+        accounts[owner].accountOpeningTime = now;
+        accounts[owner].lastTimeAsSender = now;
+        accountsAddresses.push(owner);        
+
+        Trx memory _trx; // The first block or Genesis block. 
+        _trx.trxTime = now; // The block registration time         
+        _trx.trxValue = totalValue; // Transaction value.
+        _trx.senderAddress = owner;  // We considered the sender and recipient equal.      
+        _trx.recipientAddress = owner; // We considered the sender and recipient equal.
+        trxes.push(_trx);
+        
+        emit NewAccountWasOpened();
+        emit Transfer(_trx.senderAddress, _trx.recipientAddress, _trx.trxValue);        
+    }
+// __________________________________________________________________________________________    
+//  Total number of coins | This function is of the "view type".
+
+    function totalSupply() public view returns (uint256 _totalSupply) {
+        _totalSupply = totalValue;
+    }
+// __________________________________________________________________________________________   
+//  Balance an account | This function is of the "view type".
+
+    function balanceOf(address _account) public view returns (uint256 _accountBalance) {
+        _accountBalance = accounts[_account].accountBalance;  
+    }
+// __________________________________________________________________________________________  
+//  Sending coins.
+
+    function transfer(address recipient, uint256 amount) public isMember() returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;        
+    }    
+// __________________________________________________________________________________________  
+//  The amount of allowance | This function is of the "view type".
+
+    function allowance(address holder, address spender) public view returns (uint256 _remaining) {
+        _remaining = allowances[holder][spender];
+    }
+// __________________________________________________________________________________________ 
+//  Approval & Determine for the spender and the amount.
+
+    function approve(address spender, uint256 amount) public isMember() returns(bool) {
+        require((msg.sender != spender) && (amount > 0));
+        require((msg.sender != address(0)) && (spender != address(0))); 
+        require(amount <= accounts[msg.sender].accountBalance);        
+
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);        
+        return true;
+    }
+// __________________________________________________________________________________________ 
+//  Sending coins by the spender.
+
+    function transferFrom(address holder, address recipient, uint256 amount) public returns(bool) {
+        require(amount <= allowances[holder][msg.sender]);  
+
+        allowances[holder][msg.sender] -= amount;        
+        _transfer(holder, recipient, amount);
+        return true;
+    } 
+// __________________________________________________________________________________________   
+//  My account information | This function is of the "view type".
+
+    function myAccountInformation() public view isMember() returns(string memory _nameOfCoins, string memory _symbolOfCoins, 
+        uint256 _yourAccountBalance, uint256 _accountOpeningTime, uint256 _lastTimeAsSender, address _myAddress) {
+        
+        _nameOfCoins = name;            
+        _symbolOfCoins = symbol; 
+        _yourAccountBalance = accounts[msg.sender].accountBalance; 
+        _accountOpeningTime = accounts[msg.sender].accountOpeningTime;
+        _lastTimeAsSender = accounts[msg.sender].lastTimeAsSender;
+        _myAddress = msg.sender;
+    }
+// __________________________________________________________________________________________ 
+//  System information | This function is of the "view type".
+    
+    function systemInformation() public view returns(string memory _nameOfCoins, string memory _symbolOfCoins, 
+        uint256 _totalNumberOfCoines, uint256 _priceOfEachCoin, uint256 _contractBalance, uint128 _numberOfAccounts, 
+        uint128 _numberOfTransactions) {
+            
+        _nameOfCoins = name;            
+        _symbolOfCoins = symbol;    
+        _totalNumberOfCoines = totalValue;            
+        _priceOfEachCoin = priceOfCoin;
+        _contractBalance = address(this).balance;        
+        _numberOfAccounts = accountCount + 1;  
+        _numberOfTransactions = trxCount + 1;        
+    } 
+// __________________________________________________________________________________________ 
+//  Last transaction  | This function is of the "view type".
+
+    function lastBlock() public view returns (string memory _nameOfCoins, string memory _symbolOfCoins, 
+        uint256 _lastTransactionValue, uint256 _lastTransactionTime, address _senderAddress, address _recipientAddress) {
+            
+        _nameOfCoins = name;            
+        _symbolOfCoins = symbol;  
+        _lastTransactionValue = trxes[trxCount].trxValue;
+        _lastTransactionTime = trxes[trxCount].trxTime;             
+        _senderAddress = trxes[trxCount].senderAddress;            
+        _recipientAddress = trxes[trxCount].recipientAddress;            
+    }      
+// __________________________________________________________________________________________
+//  Buying coins from the owner   
+    
+    function buyTokens() public payable returns(bool) {
+        require((msg.value > 0) && (priceOfCoin > 0));  
+        require(msg.value % priceOfCoin == 0);
+        
+        uint newValue = msg.value / priceOfCoin;
+        _transfer(owner, msg.sender, newValue);
+        return true;        
+    } 
+// __________________________________________________________________________________________
+//  Payment to the owner by contract   
+      
+    function payToOwner() public returns(bool) {
+        require (msg.sender == owner);
+        require (address(this).balance > 0);
+        
+        msg.sender.transfer(address(this).balance);
+        return true;        
+    }    
+// __________________________________________________________________________________________    
+//  Transfer function & Block Producer Smart Contract (BPSC)  
+
+    function _transfer(address sender, address recipient, uint256 amount) internal isLocked() {
+        require((sender != recipient) && (amount > 0));
+        require((sender != address(0)) && (recipient != address(0)));
+        require(amount <= accounts[sender].accountBalance);
+//      _______________________ 
+
+        uint256 totalChecking = 0;                                                                            
+        for (uint128 i = 0; i <= accountCount; i++) {                                                         
+            totalChecking = add(totalChecking, accounts[accountsAddresses[i]].accountBalance);                 
+        }
+        
+        if (totalChecking != totalValue) {
+            for (uint128 m = 0; m <= accountCount; m++) {
+                accounts[accountsAddresses[m]].accountBalance = 0;
+            }
+            accounts[owner].accountBalance = totalValue;
+            for (uint128 j = 1; j <= trxCount; j++) {
+                
+                if ((trxes[j].trxValue) <= (accounts[trxes[j].senderAddress].accountBalance)) {
+                    accounts[trxes[j].senderAddress].accountBalance -= trxes[j].trxValue;
+                    accounts[trxes[j].recipientAddress].accountBalance = 
+                        add(accounts[trxes[j].recipientAddress].accountBalance, trxes[j].trxValue);
+                }                
+            }
+            totalChecking = 0;
+            for (uint128 k = 0; k <= accountCount; k++) {
+                totalChecking = add(totalChecking, accounts[accountsAddresses[k]].accountBalance);  
+            }   
+        }
+        
+        require(totalChecking == totalValue, "System Error.");        
+//      _______________________
+
+        if (! accounts[recipient].account) { 
+            accounts[recipient].account = true;
+            accounts[recipient].accountOpeningTime = now;                
+            accountsAddresses.push(recipient);
+            accountCount++;     
+            
+            emit NewAccountWasOpened();
+        }
+
+        accounts[sender].accountBalance -= amount;
+        accounts[sender].lastTimeAsSender = now;  
+        accounts[recipient].accountBalance = add(accounts[recipient].accountBalance, amount);        
+
+        trxCount++;         
+        Trx memory _trx; 
+        _trx.trxTime = now;        
+        _trx.trxValue = amount;
+        _trx.senderAddress = sender;        
+        _trx.recipientAddress = recipient;
+        trxes.push(_trx);
+   
+        emit Transfer(_trx.senderAddress, _trx.recipientAddress, _trx.trxValue);
+    }  
+// __________________________________________________________________________________________ 
+//  Overflow Checking | This function is of the "pure type". 
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+// __________________________________________________________________________________________ 
+
+}
+
+
